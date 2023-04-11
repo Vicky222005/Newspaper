b590b7d17b5ed71722c96dca9490aa80216e5e38
Win Htet Aung <winhtetaung@Wins-MacBook-Pro.local>
2023-04-11 17:55:59 +0630

    Newspaper

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+class CustomUser(AbstractUser):
+    age = models.PositiveBigIntegerField(null=True, blank=True)
+# Create your models here.

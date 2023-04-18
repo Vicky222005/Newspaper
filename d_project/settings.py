a0cfec2d8876cc7ee67c8788b849f02ecf3a70d3
Win Htet Aung <winhtetaung@Wins-MacBook-Pro.local>
2023-04-18 17:25:45 +0630

    Newspaper

--- d_project/settings.py
+++ d_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
-    'articles.apps.ArticlesCongif',
+    'articles.apps.ArticlesConfig',
 ]
 
 MIDDLEWARE = [
